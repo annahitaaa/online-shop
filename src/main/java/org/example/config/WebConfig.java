3fdbfdfad61f1f253ee05e5c03ff9774316518d2
Annahita <aalipourannahita@gmail.com>
2024-11-12 17:52:08 +0330

    spring web mvc start

---
+++ src/main/java/org/example/config/WebConfig.java
@@ -0,0 +1,25 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan("org.example.controllers")
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver= new InternalResourceViewResolver();
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+}
