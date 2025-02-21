e9b89feb092001cd34da74d381f1e6fbb76e7d6d
Annahita Aalipour <aalipourannhita@gmail.com>
2025-02-21 22:20:02 +0330

    adding Swagger

--- src/main/java/org/example/validators/PhoneValidator.java
+++ src/main/java/org/example/validators/PhoneValidator.java
@@ -14,9 +14,7 @@ public class PhoneValidator implements ConstraintValidator<Phone, String> {
     @Override
     public boolean isValid(String phoneNumber, ConstraintValidatorContext constraintValidatorContext) {
         if(phoneNumber == null)
-        return false;
-        if(phoneNumber.matches("09[1-5][1-9]\\d{7}$"))
-            return true;
-        return false;
+         return false;
+        return phoneNumber.matches("09[1-5][1-9]\\d{7}$");
     }
 }
